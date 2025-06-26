c7da11bb06a929bc8632b4c7b8fefcc6c8f5025f
Felix Derler <felixderler60@gmail.com>
2025-06-26 15:31:41 +0200

    build.py aktualisieren

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 import PyInstaller.__main__
 import os
 
-# Get the absolute path to the script's directory
+
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 PyInstaller.__main__.run([
@@ -9,6 +9,5 @@ PyInstaller.__main__.run([
     '--onefile',
     '--windowed',
     '--name', 'HashcatGUI',
-    # You can specify an icon file here
-    # '--icon', os.path.join(script_dir, 'icon.ico'),
+    
 ])
